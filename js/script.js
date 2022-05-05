3967e158f1f2506fc5e1c97a786e05848ab7e026
Stepan <solo_gosu@hotmail.com>
2022-05-05 23:54:42 +0300

    nc

--- js/script.js
+++ js/script.js
@@ -24,24 +24,4 @@ navToggle.addEventListener('click', () => {
         overlay.classList.remove('fade-out');
         overlay.classList.add('fade-in');
     }
-
-    // if (navToggle.classList.contains('open')) {
-    //     navToggle.classList.remove('open');
-    //     nav.classList.remove('open');
-
-    //     links.classList.remove('fade-in');
-    //     links.classList.add('fade-out');
-
-    //     overlay.classList.remove('fade-in');
-    //     overlay.classList.add('fade-out');
-    // } else {
-    //     navToggle.classList.add('open');
-    //     nav.classList.add('open');
-
-    //     links.classList.remove('fade-out');
-    //     links.classList.add('fade-in');
-
-    //     overlay.classList.remove('fade-out');
-    //     overlay.classList.add('fade-in');
-    // }
 });
